99e3837f36d5a0acad22b5bb1195ac2def9fac62
Daniel Verdejo <Daniel Verdejo>
2016-10-10 10:44:10 +0100

    updated hello.py with new route 3rd commit

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
+# reference http://flask.pocoo.org/
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/name")
+def name()
+    return "Hello you"
 
 if __name__ == "__main__":
     app.run()
